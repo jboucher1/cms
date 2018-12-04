233137103687548f8147376fb50f53bb7cb81032
Jess Boucher <boucherjess@gmail.com>
2018-12-04 13:58:57 +1300

    heroku config

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to eventCms!');
+    expect(page.getTitleText()).toEqual('Welcome to cms!');
   });
 });
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'eventCms'`, () => {
+  it(`should have as title 'cms'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('eventCms');
+    expect(app.title).toEqual('cms');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to eventCms!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to cms!');
   });
 });
